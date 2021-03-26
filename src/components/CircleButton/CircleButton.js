31e464a043f68315245ebeb166aac24280850dd7
Mainak Ghosh <contactwithmainak@gmail.com>
2021-03-26 22:38:20 +0530

    Few components added...

---
+++ src/components/CircleButton/CircleButton.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CircleButton = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 250 250"
+        width="35"
+        height="35"
+      >
+        <circle
+          cx="100"
+          cy="100"
+          r="50"
+          fill="gold"
+          id="circle"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default CircleButton;
